dd9c53d53bb2fc3ca3dc3873cf86518535eabcaa
Nick Deligiannis <nd.serv.acc@gmail.com>
2024-09-18 18:21:31 +0300

    Added setup.py

---
+++ src/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="testcrush",
+    description="An assembly-STL compaction toolkit based on VCS-Z01X.",
+    version="0.5.0",
+    packages=find_packages(),
+    install_requires=[],
+    url="https://github.com/cad-polito-it/testcrush",
+    licence="MIT",
+    author="Nick Deligiannis",
+    author_email="nd.serv.acc@gmail.com",
+    maintainer="Nick Deligiannis",
+    maintainer_email="nd.serv.acc@gmail.com",
+    include_package_data=True,
+)
