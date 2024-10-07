81fb1ef8357cdf0684cb922b3653286f9c4f96fa
Nick Deligiannis <nd.serv.acc@gmail.com>
2024-10-07 11:42:55 +0300

    Updated unit tests for grammars/transformers.py

--- src/unit_tests/test_transformers.py
+++ src/unit_tests/test_transformers.py
@@ -14,8 +14,6 @@ except ModuleNotFoundError:
     from testcrush import zoix
 
 import unittest
-import unittest.mock as mock
-import pathlib
 import lark
 
 
@@ -119,3 +117,93 @@ class FaultListTransformerTest(unittest.TestCase):
         ]
 
         self.assertEqual(fault_list, expected_faults)
+
+class TraceTransformerCV32E40PTest(unittest.TestCase):
+
+    grammar = open("../testcrush/grammars/trace_cv32e40p.lark").read()
+
+    def get_parser(self):
+
+        return lark.Lark(grammar=self.grammar, start="start", parser="lalr", transformer=transformers.TraceTransformerCV32E40P())
+
+    def test_doc_example(self):
+
+        parser = self.get_parser()
+
+        trace_sample = r"""Time          Cycle      PC       Instr    Decoded instruction Register and memory contents
+130         61 00000150 4481     c.li    x9,0        x9=0x00000000
+132         62 00000152 00008437 lui     x8,0x8      x8=0x00008000
+134         63 00000156 fff40413 addi    x8,x8,-1    x8:0x00008000  x8=0x00007fff
+136         64 0000015a 8c65     c.and   x8,x9       x8:0x00007fff  x9:0x00000000  x8=0x00000000
+142         67 0000015c c622     c.swsp  x8,12(x2)   x2:0x00002000  x8:0x00000000 PA:0x0000200c store:0x00000000  load:0xffffffff
+"""
+        csv_lines = parser.parse(trace_sample)
+
+        expected_csv_lines = ['Time,Cycle,PC,Instr,Decoded instruction,Register and memory contents',
+                              '130,61,00000150,4481,"c.li x9,0","x9=0x00000000"',
+                              '132,62,00000152,00008437,"lui x8,0x8","x8=0x00008000"',
+                              '134,63,00000156,fff40413,"addi x8,x8,-1","x8:0x00008000, x8=0x00007fff"',
+                              '136,64,0000015a,8c65,"c.and x8,x9","x8:0x00007fff, x9:0x00000000, x8=0x00000000"',
+                              '142,67,0000015c,c622,"c.swsp x8,12(x2)","x2:0x00002000, x8:0x00000000, PA:0x0000200c, store:0x00000000, load:0xffffffff"']
+
+        self.assertEqual(csv_lines, expected_csv_lines)
+
+    def test_no_reg_and_mem_segment(self):
+
+        parser = self.get_parser()
+
+        trace_sample = r"""Time    Cycle   PC  Instr   Decoded instruction Register and memory contents
+    905ns              86 00000e36 00a005b3 c.add            x11,  x0, x10       x11=00000e5c x10:00000e5c
+    915ns              87 00000e38 00000693 c.addi           x13,  x0, 0         x13=00000000
+    925ns              88 00000e3a 00000613 c.addi           x12,  x0, 0
+    935ns              89 00000e3c 00000513 c.addi           x10,  x0, 0
+    945ns              90 00000e3e 2b40006f c.jal             x0, 692
+    975ns              93 000010f2 0d01a703 lw               x14, 208(x3)        x14=00002b20  x3:00003288  PA:00003358
+    985ns              94 000010f6 00a00333 c.add             x6,  x0, x10        x6=00000000 x10:00000000
+    995ns              95 000010f8 14872783 lw               x15, 328(x14)       x15=00000000 x14:00002b20  PA:00002c68
+   1015ns              97 000010fc 00079563 c.bne            x15,  x0, 10        x15:00000000
+"""
+        csv_lines = parser.parse(trace_sample)
+
+        expected_csv_lines = ['Time,Cycle,PC,Instr,Decoded instruction,Register and memory contents',
+                              '905ns,86,00000e36,00a005b3,"c.add x11, x0, x10","x11=00000e5c, x10:00000e5c"',
+                              '915ns,87,00000e38,00000693,"c.addi x13, x0, 0","x13=00000000"',
+                              '925ns,88,00000e3a,00000613,"c.addi x12, x0, 0"',
+                              '935ns,89,00000e3c,00000513,"c.addi x10, x0, 0"',
+                              '945ns,90,00000e3e,2b40006f,"c.jal x0, 692"',
+                              '975ns,93,000010f2,0d01a703,"lw x14, 208(x3)","x14=00002b20, x3:00003288, PA:00003358"',
+                              '985ns,94,000010f6,00a00333,"c.add x6, x0, x10","x6=00000000, x10:00000000"',
+                              '995ns,95,000010f8,14872783,"lw x15, 328(x14)","x15=00000000, x14:00002b20, PA:00002c68"',
+                              '1015ns,97,000010fc,00079563,"c.bne x15, x0, 10","x15:00000000"']
+
+        self.assertEqual(csv_lines, expected_csv_lines)
+
+    def test_no_operands_in_decoded_instruction_and_no_reg_and_mem(self):
+
+        parser = self.get_parser()
+
+        trace_sample = r"""Time    Cycle   PC  Instr   Decoded instruction Register and memory contents
+    905ns              86 00000e36 00a005b3 c.add                   x11=00000e5c x10:00000e5c
+    915ns              87 00000e38 00000693 c.addi                  x13=00000000
+    925ns              88 00000e3a 00000613 c.addi
+    935ns              89 00000e3c 00000513 c.addi           x10,  x0, 0
+    945ns              90 00000e3e 2b40006f c.jal             x0, 692
+    975ns              93 000010f2 0d01a703 lw               x14, 208(x3)        x14=00002b20  x3:00003288  PA:00003358
+    985ns              94 000010f6 00a00333 c.add             x6,  x0, x10        x6=00000000 x10:00000000
+    995ns              95 000010f8 14872783 lw               x15, 328(x14)       x15=00000000 x14:00002b20  PA:00002c68
+   1015ns              97 000010fc 00079563 c.bne            x15,  x0, 10        x15:00000000
+"""
+        csv_lines = parser.parse(trace_sample)
+
+        expected_csv_lines = ['Time,Cycle,PC,Instr,Decoded instruction,Register and memory contents',
+                              '905ns,86,00000e36,00a005b3,"c.add","x11=00000e5c, x10:00000e5c"',
+                              '915ns,87,00000e38,00000693,"c.addi","x13=00000000"',
+                              '925ns,88,00000e3a,00000613,"c.addi"',
+                              '935ns,89,00000e3c,00000513,"c.addi x10, x0, 0"',
+                              '945ns,90,00000e3e,2b40006f,"c.jal x0, 692"',
+                              '975ns,93,000010f2,0d01a703,"lw x14, 208(x3)","x14=00002b20, x3:00003288, PA:00003358"',
+                              '985ns,94,000010f6,00a00333,"c.add x6, x0, x10","x6=00000000, x10:00000000"',
+                              '995ns,95,000010f8,14872783,"lw x15, 328(x14)","x15=00000000, x14:00002b20, PA:00002c68"',
+                              '1015ns,97,000010fc,00079563,"c.bne x15, x0, 10","x15:00000000"']
+
+        self.assertEqual(csv_lines, expected_csv_lines)
